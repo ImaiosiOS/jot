e04a7d500d8f52fc035e5c8df4b8cb19e6fb7498
ImaiosiOS <imaios.ios@imaios.com>
2026-04-03 07:52:52 +0000

    added headers

--- jot/include/jot.h
+++ jot/include/jot.h
@@ -6,4 +6,37 @@
 //
 //
 
+#import <Foundation/Foundation.h>
+
+// Core Jot
 #import "JotViewController.h"
+#import "JotDrawingContainer.h"
+#import "JotDrawView.h"
+#import "JotTextEditView.h"
+#import "JotTextView.h"
+
+// Touch handling
+#import "JotTouchBezier.h"
+#import "JotTouchPoint.h"
+
+// Categories
+#import "UIImage+Jot.h"
+
+// Masonry (since bundled in your include)
+#import "MASConstraint.h"
+#import "MASConstraintMaker.h"
+#import "MASConstraint+Private.h"
+#import "MASConstraintMaker.h"
+#import "MASConstraintMaker.h"
+#import "MASCompositeConstraint.h"
+#import "MASLayoutConstraint.h"
+#import "MASUtilities.h"
+#import "MASViewAttribute.h"
+#import "MASViewConstraint.h"
+
+// Masonry categories
+#import "NSArray+MASAdditions.h"
+#import "NSArray+MASShorthandAdditions.h"
+#import "NSLayoutConstraint+MASDebugAdditions.h"
+#import "View+MASAdditions.h"
+#import "View+MASShorthandAdditions.h"
